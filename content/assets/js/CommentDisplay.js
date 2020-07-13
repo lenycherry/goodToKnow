5c6181134a5a39f45936b1e0dc6f3b2aecbc54f4
lenycherry <lenoircelia@hotmail.fr>
2020-07-13 18:46:38 +0200

    add: async request & interval func for comments refresh

--- content/assets/js/CommentDisplay.js
+++ content/assets/js/CommentDisplay.js
@@ -6,12 +6,18 @@ class CommentDisplay {
         this.editUrl = document.getElementById('url_host').innerText + "editComment/id/"
         this.deleteUrl = document.getElementById('url_host').innerText + "deleteComment/id/"
         this.pseudo = document.getElementById('json_pseudo').innerText
+        this.currentJson
+
         this.findJson()
+        this.intervalResfresh = setInterval(e => this.refreshJson(), 8000);
+
     }
     // requête ajax pour afficher les commentaires dynamiquement dans le Html.
     findJson = async function () {
         let response = await fetch(this.jsonUrl)
         let comments = await response.json()
+        this.currentJson = comments
+
         comments.forEach(comment => {
             let commentContainer = document.createElement("div")
             commentContainer.classList.add("article_comment_container")
@@ -35,13 +41,13 @@ class CommentDisplay {
                 editDateComment.innerText = comment.edit_date
                 commentContainer.appendChild(editDateComment)
             }
-            console.log(this.pseudo, comment.pseudo)
-            if(this.pseudo != "" && this.pseudo != comment.pseudo){
-            let reportBtn = document.createElement("a")
-            reportBtn.classList.add('GTK_alert', 'report_com_btn', 'btn')
-            reportBtn.innerText = "Signaler"
-            reportBtn.href = this.reportUrl + comment.id
-            commentContainer.appendChild(reportBtn)
+
+            if (this.pseudo != "" && this.pseudo != comment.pseudo) {
+                let reportBtn = document.createElement("a")
+                reportBtn.classList.add('GTK_alert', 'report_com_btn', 'btn')
+                reportBtn.innerText = "Signaler"
+                reportBtn.href = this.reportUrl + comment.id
+                commentContainer.appendChild(reportBtn)
             }
             if (this.pseudo == comment.pseudo) {
                 let editBtn = document.createElement("a")
@@ -56,12 +62,23 @@ class CommentDisplay {
                 deleteBtn.href = this.deleteUrl + comment.id
                 commentContainer.appendChild(deleteBtn)
             }
-
             this.containerJson.appendChild(commentContainer)
         });
         //initialise un nouvel objet alert à la fin de la requête ajax afin que le message d'alerte fonctionne correctement
         let alertAjax = new Alert()
-
+    }
+    //refresh du Json avec une comparaison entre le fichier avant/après la requête, si il y a un changement on réinitialise l'affichage.
+    refreshJson = async function () {
+        let response = await fetch(this.jsonUrl)
+        let comments = await response.json()
+        if (comments.length != this.currentJson.length) {
+            let containerJson = document.querySelector('#comments_container_json')
+            let jsonChild = document.querySelectorAll(".article_comment_container")
+            for (let i = 0; i < jsonChild.length; i++) {
+                containerJson.removeChild(jsonChild[i])
+            }
+            this.findJson()
+        }
     }
 }
 
