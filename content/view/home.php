3f6b65f38ac7c7c6f0dc2b46b96bc57f2333ee28
lenycherry <lenoircelia@hotmail.fr>
2020-07-15 17:51:14 +0200

    style : home css improvement

--- content/view/home.php
+++ content/view/home.php
@@ -3,30 +3,25 @@
   <h1 class="reveal">GOOD TO KNOW</h1>
   <h3 class="reveal">Apprendre pour mieux comprendre notre environnement.</h3>
 </div>
-<div id='main_home_content'>
-
-  <div id="new_article_container" class="reveal">
-    <?php $lastArticle = end($articles); ?>
-
-    <img alt="<?php echo $lastArticle['title']; ?>" class="img_last_article" src="<?php echo $lastArticle['imageUrl']; ?>" />
-
+<div id='main_home_content' class="reveal">
+  <?php $lastArticle = end($articles); ?>
+  <h2><a href="<?php echo HOST; ?>article/id/<?php echo $lastArticle['id'] ?>"><?php echo $lastArticle['title']; ?></a></h2>
+  <div id="new_article_container"> 
     <article class="new_article_content">
-    <h2><a href="<?php echo HOST; ?>article/id/<?php echo $lastArticle['id'] ?>">Dernier article publié</a></h2>
-      <h3><?php echo $lastArticle['title']; ?></h3>
       <p><?php echo substr($lastArticle['content'], 0, 500); ?> <p>...</p>
-      <a class="btn" href="<?php echo HOST; ?>article/id/<?php echo $lastArticle['id'] ?>">Voir l'article</a>
     </article>
+    <img alt="<?php echo $lastArticle['title']; ?>" class="img_last_article" src="<?php echo $lastArticle['imageUrl']; ?>" />
   </div>
 
   <div id="home_articles" class="reveal">
-  <div class="list_content_articles">
-    <?php foreach ($articles as $article) : ?><a href="<?php echo HOST; ?>article/id/<?php echo $article['id'] ?>">
-      <article class="article_content_container invisible">
-      <img alt="<?php echo $article['title']; ?>" class="img_resume_article" src="<?php echo $article['imageUrl']; ?>" />
-        <h3><?php echo $article['title']; ?></h3>
-    </article>
-    </a>
-    <?php endforeach; ?>
+    <div class="list_content_articles">
+      <?php foreach ($articles as $article) : ?><a href="<?php echo HOST; ?>article/id/<?php echo $article['id'] ?>">
+          <article class="article_content_container invisible">
+            <img alt="<?php echo $article['title']; ?>" class="img_resume_article" src="<?php echo $article['imageUrl']; ?>" />
+            <h3><?php echo $article['title']; ?></h3>
+          </article>
+        </a>
+      <?php endforeach; ?>
     </div>
     <div>
       <button class="first_page_btn btn">|<</button> <button class="previous_page_btn btn">
@@ -35,7 +30,7 @@
             <button class="last_page_btn btn">>|</button>
     </div>
   </div>
-  
+
 
 </div>
 
